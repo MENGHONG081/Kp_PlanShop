4818b81c01ed5de56107e113cd78e14b2aabbaeb
MENGHONG081 <151207103+MENGHONG081@users.noreply.github.com>
2026-02-20 19:52:25 +0700

    Update log1.php

--- User_Page/plant_admin/log1.php
+++ User_Page/plant_admin/log1.php
@@ -276,8 +276,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
           <i class="fas fa-arrow-right me-2"></i>Enter Garden
         </button>
       </form>
-      <a href="../index1.php" class="btn btn-link mt-3 d-block text-center">Back to Main Site</a>
-      <a href="login.php" class="btn btn-link mt-3 d-block text-center"> Change UI </a>
+      <a href="../index1.php">Back to Main Site</a>
+      <a href="login.php""> Change UI </a>
     </div>
   </div>
 </div>
